7527fc113b1543ccc898f9a79e7ba3649e0cba2c
Pradeep Voleti <pradeep.voleti@compass.com>
2022-12-20 15:25:42 +0530

    Initial Commit

---
+++ MobileLearning/MobileLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  MobileLearningApp.swift
+//  MobileLearning
+//
+//  Created by Pradeep Voleti on 20/12/22.
+//
+
+import SwiftUI
+
+@main
+struct MobileLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
